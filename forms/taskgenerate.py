141baeee47bb13026c549ac08c639a2f1e5f79fe
Aleks <Aleksandrcryu4kov@yandex.ru>
2023-04-25 19:33:54 +0300

    form ready but data no go to db

--- forms/taskgenerate.py
+++ forms/taskgenerate.py
@@ -4,21 +4,22 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, InputRequir
 
 
 class TaskAnswerForm(FlaskForm):
-    answer = StringField('Вариант', validators=[DataRequired()])
+    answer = StringField('', validators=[DataRequired()])
 
 
 class TaskForm(FlaskForm):
     condition = TextAreaField('Условие', validators=[DataRequired()])
     condition_file = FileField('Файл условия')
-    answers = FieldList(FormField(TaskAnswerForm), min_entries=10)
+    answers = FieldList(FormField(TaskAnswerForm), min_entries=4)
     true_answer = StringField('Верный ответ', validators=[DataRequired()])
     type_answer = SelectField('Тип ответа', coerce=int, choices=[
         (1, 'Строка'),
         (2, 'Выбор из вариантов'),
-        (3, 'Один выбор из вариантов')
-    ])
+        (3, 'Один выбор из вариантов')])
+
 
 
 class TaskGenerateForm(FlaskForm):
+    task_restart = SubmitField('Обновить страницу', render_kw={'formnovalidate': True})
     do_test_task = SubmitField('Сделать тест', render_kw={'formnovalidate': True})
     tasks_list = FieldList(FormField(TaskForm), min_entries=100)

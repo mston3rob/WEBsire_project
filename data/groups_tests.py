ec3c563fcc61de087a71dd96ce1f087204bab54c
mston3rob <shrekkamnev@gmail.com>
2023-04-25 15:03:55 +0300

    refactor tests tables db

--- data/groups_tests.py
+++ data/groups_tests.py
@@ -4,13 +4,13 @@ from .db_session import SqlAlchemyBase
 from sqlalchemy_serializer import SerializerMixin
 
 
-class Group(SqlAlchemyBase, UserMixin, SerializerMixin):
+class GroupTest(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'groups_tests'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    id_questions = sqlalchemy.Column(sqlalchemy.String, nullable=True) # id вопросов
-    login = sqlalchemy.Column(sqlalchemy.Integer, nullable=True) # login группы для выкладывания
+    id_teacher = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)  # id учителя что выложил
+    id_group = sqlalchemy.Column(sqlalchemy.String, nullable=True)  # id группы для выкладывания
+    id_questions = sqlalchemy.Column(sqlalchemy.String, nullable=True) # id вопросов (через ;)
     block_list = sqlalchemy.Column(sqlalchemy.String, nullable=True) # если тест не для всех учеников группы (исключение)
     count = sqlalchemy.Column(sqlalchemy.Integer, nullable=True) # количество прохождений
     time = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True) # время для прохождения
-
--- data/tests.py
+++ data/tests.py
@@ -4,12 +4,12 @@ from .db_session import SqlAlchemyBase
 from sqlalchemy_serializer import SerializerMixin
 
 
-class Group(SqlAlchemyBase, UserMixin, SerializerMixin):
+class Answer(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'tests'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     question = sqlalchemy.Column(sqlalchemy.String, index=True, nullable=True) # вопрос
     variant_answers = sqlalchemy.Column(sqlalchemy.String, index=True, nullable=True) # все варианты ответа
-    correct_answer = sqlalchemy.Column(sqlalchemy.String, index=True, nullable=True) # правильный ответ(ы)
+    correct_answer = sqlalchemy.Column(sqlalchemy.String, index=True, nullable=True) #  правильный ответ(ы)
     points = sqlalchemy.Column(sqlalchemy.Integer, index=True, nullable=True) # баллы за ответ
     img = sqlalchemy.Column(sqlalchemy.BLOB, index=True, nullable=True) # изобрадение к тесту
